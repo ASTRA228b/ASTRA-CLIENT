bc80bbf1a2d3da5da181da814899cb8710085076
ASTRA228b <hackerzvr4@gmail.com>
2025-08-30 18:19:24 -0500

    Add files via upload

---
+++ Patch.cs
@@ -0,0 +1,10 @@
+﻿using HarmonyLib;
+
+public class HarmonyPatch1 
+{
+    public static void INTI()
+    {
+        var harmony = new Harmony(ASTRA_CLIENT.main.PluginInfo.GUID);
+        harmony.PatchAll();
+    }
+}
